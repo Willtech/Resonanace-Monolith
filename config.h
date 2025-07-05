7844ecee82365113d4b80b1c6280188de147ad22
Willtech <willtech@live.com.au>
2025-07-06 09:18:44 +1000

    Create config.h
    Initial commit.

---
+++ config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Wi-Fi credentials
+#define WIFI_SSID     "YourNetworkName"
+#define WIFI_PASS     "YourPassword"
+
+// LED config
+#define LED_PIN       5
+#define NUM_LEDS      60
+#define BRIGHTNESS    150
+
+// Charging input pin (active HIGH)
+#define CHARGING_PIN  15
+
+// EEPROM addresses
+#define ADDR_MODE     0
+#define ADDR_BRIGHT   10
+#define ADDR_COLOR    20
